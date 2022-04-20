9277ec598ca6d086efe639a3b92bb12c5d52e6fa
unknown <juniorgomes0806@gmail.com>
2022-04-19 21:45:33 -0300

    escrevendo sobre arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,73 @@
+
+// Exercício de interpretação 1
+
+//1.a> Vai dar indefinido pois não tem valor atribuido 
+//1.b> Vai dar (null) porque o valor atribuido a array é null
+//1.c> Tem 11 números
+//1.d> O valor deu 3, porque foi atribuido a valor da vareavel acima
+//2.e> Porque foi pedido pra imprimir o valor do (0)>(i) até o 19
+//3.f> Iprimiu o 9 porque somou os 2 valores
+
+// Exercício de interpretação 2
+// Resposta = 27 e todas maiúculas
+  
+
+
+// Exercício de escrita de código 1
+
+/*let email1 = prompt ('Qual é o seu email?')
+let nome1 = prompt ('Qual é o seu nome ?')
+
+console.log ('o seu email juniorgomes0608@gmail.com foi cadastrado com sucesso' , 'Bem vindo junior')*/
+
+
+
+// Exercício escrita de código
+
+/*1-Escrita>
+1.a>let email = prompt ("email")
+1.b> let usuario = prompt ("usuario")
+1.c> console.log("O e-mail juniorgomes0608@gmail.com foi cadastrado com sucesso. Seja bem-vinda , junior!")*/
+
+
+/*let minhasComidas = ["baião", "farofa", "arroz", "pizza", "pastel"]
+console.log (minhasComidas)
+
+let preferidas =("Essas são minhas comidas preferidas")
+
+console.log(preferidas)
+console.log(minhasComidas[0])
+console.log(minhasComidas[1])
+console.log(minhasComidas[2])
+console.log(minhasComidas[3])
+
+let comidaPreferida = prompt("Qual a sua comida preferida?")
+
+minhasComidas[1] = comidaPreferida
+
+console.log(minhasComidas[1])*/
+
+
+/*3-Escrita 
+
+let listaDeTarefas = []
+
+let tarefa = prompt("primeira tarefa do dia ?")
+listaDeTarefas.push(tarefa)
+
+let tarefa2 = prompt ("segunda tarefa do dia ?")
+listaDeTarefas.push(tarefa2) 
+
+let tarefa3 = prompt("terceira tarefa do dia ?")
+listaDeTarefas.push(tarefa3)
+
+console.log(listaDeTarefas[0])
+console.log(listaDeTarefas[1])  
+console.log(listaDeTarefas[2])
+
+let efetuadas = prompt("Qual voce ja fez ?")
+let concluiu = listaDeTarefas.pop(- 1)
+
+console.log(listaDeTarefas)*/
+
+
